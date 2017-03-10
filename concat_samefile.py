a394c476470475e22aa9e921e6322c241cd383d3
Swall0w <technext.jpn@gmail.com>
2017-03-10 14:06:24 +0900

    file opens

--- concat_samefile.py
+++ concat_samefile.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import os.path
+import difflib
 
 def main():
     original_dir = 'separate_jornaldata/'
@@ -9,6 +10,12 @@ def main():
     append_filelist = [item for item in os.listdir(append_dir) if os.path.splitext(item)[1] == '.csv']
     print(append_filelist)
     print(len(append_filelist))
+
+    item = append_filelist[0]
+    print(item)
+    with open(append_dir+item,'r') as appendfile, open(original_dir+item,'r') as originalfile:
+        appendstr = appendfile.read()
+        originalstr = originalfile.read()
 
 
 if __name__ == '__main__':
